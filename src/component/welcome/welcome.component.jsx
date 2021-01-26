348998d8e41c97b28ead58d2f5a7751cd4347179
samuray90 <ionutsdaniel53@gmail.com>
2021-01-26 12:04:26 +0200

    styled title name and paragraph

--- src/component/welcome/welcome.component.jsx
+++ src/component/welcome/welcome.component.jsx
@@ -1,12 +1,27 @@
-import React from "react";
-import { StyledTitle } from "./welcome.style";
+import React, { useEffect, useState } from "react";
+import { StyledColumn } from "../column/column.style";
+import * as SC from "./welcome.style";
 
 const Welcome = () => {
+  const [welcome, updateWelcome] = useState({});
+
+  useEffect(() => {
+    fetch("http://localhost:4000/welcome")
+      .then((result) => result.json())
+      .then((result) => updateWelcome(result));
+  }, []);
   return (
     <div>
-      <StyledTitle>Welcome</StyledTitle>
+      <SC.StyledH1>Welcome</SC.StyledH1>
+      <StyledColumn height={40} />
+      <SC.StyledName>{welcome.name}</SC.StyledName>
+      <StyledColumn height={40} />
+      <SC.StyledH3>Junior Web Developer</SC.StyledH3>
+      <StyledColumn height={40} />
+      <SC.StyledH3>About me</SC.StyledH3>
+      <StyledColumn height={40} />
+      <SC.StyledDescription>{welcome.sectionDescription}</SC.StyledDescription>
     </div>
   );
 };
-
 export { Welcome };
--- src/component/welcome/welcome.style.js
+++ src/component/welcome/welcome.style.js
@@ -14,3 +14,44 @@ export const StyledTitle = styled.div`
   font-family: monospace;
   align-items: center;
 `;
+
+export const StyledH1 = styled.h1`
+  padding: 10px;
+  height: 60px;
+  background-color: #d52027;
+  position: relative;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  color: #fff;
+`;
+
+export const StyledH3 = styled.h3`
+  font-size: 30px;
+  display: flex;
+  font-weight: 600;
+  color: #707070;
+  text-align: center;
+  justify-content: center;
+`;
+
+export const StyledName = styled.div`
+  font-size: 45px;
+  display: flex;
+  font-weight: 600;
+  color: #707070;
+  text-align: center;
+  justify-content: center;
+`;
+
+export const StyledDescription = styled.div`
+  background-color: #fff;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.25);
+  padding: 20px;
+  margin: 10px;
+  border-radius: 6px;
+  font-size: 15px;
+  color: #707070;
+  font-weight: 600;
+  line-height: 30px;
+`;
